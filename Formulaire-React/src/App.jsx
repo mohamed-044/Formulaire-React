52c65841682a4909b8d453f2c09e56434dc35a94
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-06-24 15:01:03 +0200

    add react bootsrap

--- Formulaire-React/src/App.jsx
+++ Formulaire-React/src/App.jsx
@@ -1,14 +1,56 @@
 import { useState } from 'react'
 import './App.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function App() {
-  
+
+  const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+  name: "",
+  date: "",
+  select: "Basse",
+  isCompleted: false,
+  });
+
+  const handleChange = (e) => {
+  const { name, value, type, checked } = e.target;
+  setFormData((prevData) => ({
+  ...prevData,
+  [name]: type === "checkbox" ? checked : value,
+  }));
+  };
+  const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(formData);
+  };
 
   return (
-    <>
-      
-    </>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Nom</Form.Label>
+        <Form.Control type="name" name='name' value={formData.name} onChange={handleChange} placeholder="Entrez votre nom" />
+
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="date">
+        <Form.Label>Date</Form.Label>
+        <Form.Control type="date" name='date' value={formData.date} onChange={handleChange} placeholder="Entrez votre date" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="select">
+          <Form.Select id="Select" name='select' value={formData.select} onChange={handleChange}>
+            <option>Basse</option>
+            <option>Moyenne</option>
+            <option>Haute</option>
+          </Form.Select>
+      </Form.Group>    
+      <Form.Group className="mb-3" controlId="isCompleted">
+        <Form.Check type="checkbox" name='isCompleted' value={formData.isCompleted} onChange={handleChange} label="Complété" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Envoyer
+      </Button>
+    </Form>
   )
 }
 
-export default App
+export default RegistrationForm;
